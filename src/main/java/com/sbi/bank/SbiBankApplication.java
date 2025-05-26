6e149d7d58bec20b604f7a2014dfd9a2776e8f18
Pedaiahk <pedaiahk@gmail.com>
2025-05-26 13:35:37 +0530

    main

---
+++ src/main/java/com/sbi/bank/SbiBankApplication.java
@@ -0,0 +1,13 @@
+package com.sbi.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbiBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbiBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sbi/bank/SbiBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbi.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbiBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
