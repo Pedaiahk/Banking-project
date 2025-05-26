cdb46b63801d937efb44ed8b1106ba972ea4fc25
Pedaiahk <pedaiahk@gmail.com>
2025-05-26 19:14:42 +0530

    updating package name

--- src/main/java/com/sbi/bank/controller/BankController.java
+++ src/main/java/com/sbi/bank/controller/BankController.java
@@ -1,4 +1,4 @@
-package com.sbi.bank.cntroller;
+package com.sbi.bank.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
